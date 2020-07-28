9fcb6ec77894b8a397aa54dd90aace825150d1dd
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2020-07-28 11:04:03 -0400

    MVP for Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import '../style/Home.css';
 import Logo from "../assets/logo";
@@ -7,17 +7,17 @@ import Logo from "../assets/logo";
 const Home = () => (
   <Container fluid className="home">
     <Logo className="home-img" />
-    <div className="home-links-div-outer">
-      <div className="home-links-div">
-        <Link className="home-text" to="/education">Education</Link>
-        <p>|</p>
-        <Link className="home-text" to="/experience">Experience</Link>
-        <p>|</p>
-        <Link className="home-text" to="/projects">Projects</Link>
-        <p>|</p>
-        <Link className="home-text" to="/other">Other</Link>
-      </div>
-    </div>
+      <Row className="home-links-row">
+        <Col className="home-links-col">
+          <Link className="home-text" to="/education">Education</Link>
+          <p className="home-text-sep">|</p>
+          <Link className="home-text" to="/experience">Experience</Link>
+          <p className="home-text-sep">|</p>
+          <Link className="home-text" to="/projects">Projects</Link>
+          <p className="home-text-sep">|</p>
+          <Link className="home-text" to="/other">Other</Link>
+        </Col>
+      </Row>
   </Container>
 );
 
