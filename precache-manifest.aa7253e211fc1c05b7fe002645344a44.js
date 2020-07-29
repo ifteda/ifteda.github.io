813ab5100c214534f935fbf4a2410fd9e8aecd75
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2020-07-28 21:17:14 -0400

    Updates

---
+++ precache-manifest.aa7253e211fc1c05b7fe002645344a44.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58754c3f78669253a74f71f12dc354b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43cbc6f49d392c17370a",
+    "url": "/static/css/main.f19a294f.chunk.css"
+  },
+  {
+    "revision": "f7213820af95ffe44a05",
+    "url": "/static/js/2.cfe5434e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cfe5434e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43cbc6f49d392c17370a",
+    "url": "/static/js/main.ae868439.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b697b5faeba63e4d32b4942719897ae7",
+    "url": "/static/media/CormorantGaramond-Bold.b697b5fa.ttf"
+  },
+  {
+    "revision": "19c45cf4d4c7d6852d13f1fd5a2f9e25",
+    "url": "/static/media/CormorantGaramond-BoldItalic.19c45cf4.ttf"
+  },
+  {
+    "revision": "ef405e2bf2adb8ad78d668c3c85f36b6",
+    "url": "/static/media/CormorantGaramond-Italic.ef405e2b.ttf"
+  },
+  {
+    "revision": "ba13b9d9266e1946b9a11499de074a24",
+    "url": "/static/media/CormorantGaramond-Regular.ba13b9d9.ttf"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3a8113737b373d5bccd6f71d91408d16",
+    "url": "/static/media/OpenSans-BoldItalic.3a811373.ttf"
+  },
+  {
+    "revision": "f6238deb7f40a7a03134c11fb63ad387",
+    "url": "/static/media/OpenSans-Italic.f6238deb.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
