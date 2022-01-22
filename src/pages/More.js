0f818332451810aa125c684af2f6b19040cb23c3
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2022-01-21 23:19:25 -0500

    more page

---
+++ src/pages/More.js
@@ -0,0 +1,33 @@
+import { React, useEffect, useState } from 'react';
+import { Col, Image, Row } from 'react-bootstrap';
+import { ContentPage } from './ContentPage';
+import NewsCard from '../components/NewsCard';
+import base from '../api/base';
+
+const More = () => {
+
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        base("news")
+            .select({ view: "Grid view" })
+            .eachPage((records, fetchNextPage) => {
+                setItems(records);
+                fetchNextPage();
+            })
+    }, [])
+
+    return (
+        <ContentPage>
+            <Row className="d-flex justify-content-left">
+                {items.filter(item => item.fields.hide == null || item.fields.hide == false).map(item =>
+                <Col key={item.id} lg={4} md={6} sm={12}>
+                    <NewsCard item={item.fields} />
+                </Col>
+                )}
+            </Row>
+        </ContentPage>
+    );
+};
+
+export default More;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import Education from './Education';
 import Experience from './Experience';
 import Projects from './Projects';
+import More from './More';
 
-export { Home, Education, Experience, Projects };
+export { Home, Education, Experience, Projects, More};
