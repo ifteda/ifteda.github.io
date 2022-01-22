9ba425867041168c9faeeddd93031edd0532fabd
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2022-01-21 20:47:15 -0500

    filter items + rename title -> heading

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -5,22 +5,11 @@ import base from '../api/base';
 
 const Education = () => {
 
-    const defaultItemInfo = {
-        fields: {
-            dates: "",
-            details: "",
-            detailsHTML: "",
-            img: [{ url: "" }],
-            subheading: "",
-            title: ""
-        }
-    };
-
     const [items, setItems] = useState([])
 
     const [modalShow, setModalShow] = useState(false);
 
-    const [itemInfo, setItemInfo] = useState(defaultItemInfo);
+    const [itemInfo, setItemInfo] = useState({});
 
     const handleClick = (itemInfo) => {
         setModalShow(true);
@@ -39,13 +28,13 @@ const Education = () => {
     return (
         <ContentPage>
             <Carousel fade>
-                {items.map(item => (
+                {items.filter(item => item.fields.hide == null || item.fields.hide == false).map(item => (
                     <Carousel.Item key={item.id} style={{ textAlign: 'center' }}>
                         <a onClick={() => handleClick(item.fields)}>
                             <>
                                 <Image fluid className="carousel-img" src={item.fields.img[0].url} />
                                 <Carousel.Caption>
-                                    <h3>{item.fields.title}</h3>
+                                    <h3>{item.fields.heading}</h3>
                                     <p>{item.fields.subheading}</p>
                                 </Carousel.Caption>
                             </>
@@ -61,7 +50,8 @@ const Education = () => {
                         >
                             <Modal.Header closeButton closeVariant="white">
                                 <Modal.Title id="contained-modal-title-vcenter">
-                                    <h1>{itemInfo.title}</h1>
+                                    <h1>{itemInfo.heading}</h1>
+                                    <h3>{itemInfo.subheading}</h3>
                                     <h4>{itemInfo.dates}</h4>
                                 </Modal.Title>
                             </Modal.Header>
