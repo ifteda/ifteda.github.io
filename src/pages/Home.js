f9a7b7bcbdd78ad3f4fd24a929cadfb7cb8b768a
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2022-01-28 23:14:16 -0500

    animate home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { Image } from 'react-bootstrap';
 import { NavBar } from '../components';
 import logo from '../assets/logo.svg';
+import base from '../api/base';
 import '../style/Home.css';
 
 const Home = () => {
+    const [greetings, setGreetings] = useState([])
+
+    useEffect(() => {
+        base("greeting")
+            .select({ view: "Grid view" })
+            .eachPage((records, fetchNextPage) => {
+                setGreetings(records);
+                fetchNextPage();
+            })
+    }, [])
+
     return (
         <div className="home">
             <NavBar />
-            <span className="title">
-                <text className="greeting">Hi, there. This is</text>
-                <text className="name"> ifteda</text>
-            </span>
+            <div class="text-container">
+                <div className="greeting-container">
+                    {greetings.filter(greeting => greeting.fields.hide == null || greeting.fields.hide == false).map(greeting =>
+                        <span className="greeting">{greeting.fields.greeting}</span>
+                    )}
+                </div>
+            </div>
+
             <Image className="logo" src={logo} />
         </div>
     );
