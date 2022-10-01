9841b9d42b3942f9b8ec57035149be6a46edd579
Ifteda A <33703032+ifteda@users.noreply.github.com>
2022-09-30 22:04:10 -0400

    Delete src/pages directory

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home';
-import Education from './Education';
-import Experience from './Experience';
-import Projects from './Projects';
-import More from './More';
-
-export { Home, Education, Experience, Projects, More};
