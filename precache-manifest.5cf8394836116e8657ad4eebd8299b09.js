a106787f6542c770d8a76cb5931fe1cdc2e45fb6
Ifteda <ifteda@gmail.com>
2024-01-21 20:56:28 -0500

    Updates

---
+++ precache-manifest.5cf8394836116e8657ad4eebd8299b09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059a76fd5b331612d2e5ae3bad3c982b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c1e867c4012a40ca42e",
+    "url": "/static/css/2.b19e41ca.chunk.css"
+  },
+  {
+    "revision": "c25595152a7c493d8a56",
+    "url": "/static/css/main.042c5aab.chunk.css"
+  },
+  {
+    "revision": "8c1e867c4012a40ca42e",
+    "url": "/static/js/2.c2633d0c.chunk.js"
+  },
+  {
+    "revision": "89d43ad9a4e13acb6b29fb12d3eb4028",
+    "url": "/static/js/2.c2633d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c532e0957a9813071f",
+    "url": "/static/js/3.74ce3b0f.chunk.js"
+  },
+  {
+    "revision": "c25595152a7c493d8a56",
+    "url": "/static/js/main.e450a73b.chunk.js"
+  },
+  {
+    "revision": "27b2ce6c78be2ed6ef68",
+    "url": "/static/js/runtime-main.3229acc5.js"
+  },
+  {
+    "revision": "3f20321a8a439d0dc30964b424d2336e",
+    "url": "/static/media/headshot.3f20321a.jpg"
+  },
+  {
+    "revision": "c36fd883f27fcf83cf6935dfbda5462c",
+    "url": "/static/media/logo.c36fd883.svg"
+  },
+  {
+    "revision": "da6fcb891f3b22d641bb5cbc81e8a3e7",
+    "url": "/static/media/wordmark.da6fcb89.svg"
+  }
+]);
