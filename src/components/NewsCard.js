ed9c60894c99e3a6413373c5c8cf464d15b6086e
Ifteda <ifteda@gmail.com>
2024-01-21 20:02:27 -0500

    adjust news card formatting

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -4,19 +4,24 @@ import '../style/More.css';
 
 const NewsCard = ({ item }) => {
     return (
-        <a href={item.link} target="_blank" rel="noopener noreferrer">
-            <Card>
+        <Card>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <Card.Header><br /></Card.Header>
                 <Card.Img src={item.imgURL} />
-                <Card.ImgOverlay>
-                    <Card.Title>{item.heading}</Card.Title>
-                    <Card.Text>
-                        {item.subheading}
-                        <p className="source">{item.dateFormatted}</p>
-                        <p className="source"><i>{item.source}</i></p>
-                    </Card.Text>
-                </Card.ImgOverlay>
-            </Card>
-        </a>
+            </a>
+            <Card.ImgOverlay>
+                <Card.Title>{item.heading}</Card.Title>
+                <Card.Text>
+                    {item.subheading}
+                </Card.Text>
+            </Card.ImgOverlay>
+
+            <Card.Footer>
+                <i>{item.source}</i>
+                <br />
+                {item.dateFormatted}
+            </Card.Footer>
+        </Card>
     );
 }
 
