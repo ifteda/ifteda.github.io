948ab31d40c1ea335ccad33aba38f1231d162416
Ifteda <ifteda@gmail.com>
2024-04-03 12:07:47 -0400

    fix NewCard hyperlink

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -4,24 +4,25 @@ import '../style/More.css';
 
 const NewsCard = ({ item }) => {
     return (
-        <Card>
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
+        <a className="card-link" href={item.link} target="_blank" rel="noopener noreferrer">
+            <Card>
                 <Card.Header><br /></Card.Header>
                 <Card.Img src={item.imgURL} />
-            </a>
-            <Card.ImgOverlay>
-                <Card.Title>{item.heading}</Card.Title>
-                <Card.Text>
-                    {item.subheading}
-                </Card.Text>
-            </Card.ImgOverlay>
 
-            <Card.Footer>
-                <i>{item.source}</i>
-                <br />
-                {item.dateFormatted}
-            </Card.Footer>
-        </Card>
+                <Card.ImgOverlay>
+                    <Card.Title>{item.heading}</Card.Title>
+                    <Card.Text>
+                        {item.subheading}
+                    </Card.Text>
+                </Card.ImgOverlay>
+
+                <Card.Footer>
+                    <i>{item.source}</i>
+                    <br />
+                    {item.dateFormatted}
+                </Card.Footer>
+            </Card>
+        </a>
     );
 }
 
