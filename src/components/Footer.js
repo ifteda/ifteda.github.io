9ed68e764fb8da3f044db101cc96a1a240296d67
Ifteda <ifteda@gmail.com>
2024-04-03 21:31:31 -0400

    adjust footer & shadows

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,34 +3,27 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import logo from '../assets/logo.svg';
 import '../style/Footer.css';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <Container fluid>
-                <Row>
-                    <Col className="col-logo">
-                        <img className="footer-logo" src={logo} alt="ifteda" />
-                    </Col>
-                    <Col className="col-contact">
-                        <div>Ifteda Ahmed-Syed</div>
-                        <div>
-                            <a href="mailto:contact@ifteda.com" target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faEnvelope} className="footer-icon" />
-                            </a>
-                            <a href="https://www.linkedin.com/in/ifteda/" target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faLinkedin} className="footer-icon" />
-                            </a>
-                            <a href="https://github.com/ifteda" target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faGithubSquare} className="footer-icon" />
-                            </a>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+        <Container className="footer" fluid>
+            <Row>
+                <Col className="contact">
+                    <div>
+                        <a href="mailto:contact@ifteda.com" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faEnvelope} className="footer-icon" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/ifteda/" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} className="footer-icon" />
+                        </a>
+                        <a href="https://github.com/ifteda" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithubSquare} className="footer-icon" />
+                        </a>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
